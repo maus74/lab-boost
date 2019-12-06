4ecd789fd8a799d8fcd2f34d3bedb4d74e753330
@Maus_74 <nikita.potapovich@mail.ru>
2019-12-06 23:01:12 +0300

    ura

--- cmake-build-debug/CMakeFiles/3.14.3/CompilerIdCXX/CMakeCXXCompilerId.cpp
+++ cmake-build-debug/CMakeFiles/3.14.3/CompilerIdCXX/CMakeCXXCompilerId.cpp
@@ -1183,7 +1183,8 @@ char const *info_cray = "INFO" ":" "compiler_wrapper[CrayPrgEnv]";
 #ifdef COMPILER_VERSION_MAJOR
 char const info_version[] = {
         'I', 'N', 'F', 'O', ':',
-        'c', 'o', 'm', 'p', 'i', 'l', 'e', 'r', '_', 'v', 'e', 'r', 's', 'i', 'o',
+        'c', 'o', 'm', 'p', 'i', 'l', 'e',
+        'r', '_', 'v', 'e', 'r', 's', 'i', 'o',
         'n', '[', COMPILER_VERSION_MAJOR,
 # ifdef COMPILER_VERSION_MINOR
         '.', COMPILER_VERSION_MINOR,
